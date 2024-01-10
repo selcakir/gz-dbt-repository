e7c80dabdb4711c2ee70e6a212b73f5b9c0f234a
Selcuk Cakir <selcakir86@gmail.com>
2024-01-10 15:43:09 +0000

    union relations added

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,15 +1,10 @@
-select
-  *
-from {{ref('stg_raw__adwords')}}
-union all
-select
-  *
-from {{ref('stg_raw__bing')}}
-union all
-select
-  *
-from {{ref('stg_raw__criteo')}}
-union all
-select
-  *
-from {{ref('stg_raw__facebook')}}
+{{
+    dbt_utils.union_relations(
+        relations=[
+            ref("stg_raw__adwords"),
+            ref("stg_raw__bing"),
+            ref("stg_raw__criteo"),
+            ref("stg_raw__facebook"),
+        ]
+    )
+}}
